58575d0385bee9d952291ba2d43245b8f1c32c04
Administrator <1319477288@qq.com>
2017-09-11 16:50:18 +0800

    新内容

--- app/src/main/java/com/example/administrator/my_day01_github/MainActivity.java
+++ app/src/main/java/com/example/administrator/my_day01_github/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.administrator.my_day01_github;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        initData();
+    }
+
+    private void initData() {
+        for (int i = 0; i < 10; i++) {
+
+
+        }
+    }
+
+    private void init() {
+        int a=1;
     }
 }
